a1bbcb0fbc8a826b3321bda440cf47c1ea137b1b
michaelbarton719 <mikebartoncc@gmail.com>
2026-02-05 18:30:36 -0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useMemo } from 'react';
+import { Container, Theme } from './settings/types';
+import { SecondMileWebsite } from './components/generated/SecondMileWebsite';
+
+let theme: Theme = 'dark';
+// only use 'centered' container for standalone components, never for full page apps or websites.
+let container: Container = 'none';
+
+function App() {
+  function setTheme(theme: Theme) {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  setTheme(theme);
+
+  const generatedComponent = useMemo(() => {
+    return <SecondMileWebsite />;
+  }, []);
+
+  if (container === 'centered') {
+    return (
+      <div className="h-full w-full flex flex-col items-center justify-center">
+        {generatedComponent}
+      </div>
+    );
+  } else {
+    return generatedComponent;
+  }
+}
+
+export default App;
